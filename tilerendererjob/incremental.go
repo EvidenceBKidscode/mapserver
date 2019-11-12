8c75d128563d56d5627882a9f3ee0f85c33fbed8
Pierre-Yves Rollo <dev@pyrollo.com>
2019-11-12 17:56:38 +0100

    Fix missing mapblocks in rendering

--- tilerendererjob/incremental.go
+++ tilerendererjob/incremental.go
@@ -13,8 +13,6 @@ type IncrementalRenderEvent struct {
 }
 
 func incrementalRender(ctx *app.App) {
-	clearRenderedSectors()
-
 	lastMtime := ctx.Settings.GetInt64(settings.SETTING_LAST_MTIME, 0)
 
 	fields := logrus.Fields{
