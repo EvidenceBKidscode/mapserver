811dba48af47a5b1a2e47839635e8e871173f436
Pierre-Yves Rollo <dev@pyrollo.com>
2019-11-28 14:19:55 +0100

    Prevent snapshot from being clicked while rendering

--- static/js/map/SnapShotControl.js
+++ static/js/map/SnapShotControl.js
@@ -1,21 +1,35 @@
 import '../lib/html2canvas.js';
 
 export default L.Control.extend({
+  initialize: function(wsChannel, opts) {
+      L.Control.prototype.initialize.call(this, opts);
+  },
+
+  _ongoingsnap: false,
+
   _takeSnapShot: function(e) {
+    if (this._ongoingsnap) return;
+    this._ongoingsnap = true;
+    var me = this;
     html2canvas(document.body, {
       ignoreElements: function(element) {
         return (element.className == "leaflet-control-container");
       },
     }).then(function(canvas) {
+      me._ongoingsnap = false;
       var link = document.createElement("a");
       link.download = "map.png";
       link.href = canvas.toDataURL('image/png');
       link.click();
+    }, function() {
+      // In case things went wrong
+      me._ongoingsnap = false;
     });
   },
 
   onAdd: function(map) {
     this._div = L.DomUtil.create('div', 'snapshot-control leaflet-control');
+    L.DomEvent.disableClickPropagation(this._div);
     this._button = L.DomUtil.create('a', 'snapshot-button', this._div);
     L.DomEvent.on(this._button, 'click', this._takeSnapShot, this);
     return this._div;
