eadad2785034640082c3dc763f3e79344816bf26
Pierre-Yves Rollo <dev@pyrollo.com>
2020-03-04 15:04:35 +0100

    Remove obsolete getkey function

--- mapblockaccessor/mapblockaccessor.go
+++ mapblockaccessor/mapblockaccessor.go
@@ -1,8 +1,6 @@
 package mapblockaccessor
 
 import (
-	"fmt"
-	"mapserver/coords"
 	"mapserver/db"
 	"mapserver/eventbus"
 
@@ -13,10 +11,6 @@ type MapBlockAccessor struct {
 	accessor   db.DBAccessor
 	blockcache *BlockCache
 	Eventbus   *eventbus.Eventbus
-}
-
-func getKey(pos *coords.MapBlockCoords) string {
-	return fmt.Sprintf("Coord %d/%d/%d", pos.X, pos.Y, pos.Z)
 }
 
 func NewMapBlockAccessor(accessor db.DBAccessor, expiretime, purgetime time.Duration, maxcount int) *MapBlockAccessor {
